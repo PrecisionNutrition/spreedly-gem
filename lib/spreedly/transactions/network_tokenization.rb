f7edebc0539d68a3de53c43c1aea725154fc5a90
Vince Puzzella <vincepuz@gmail.com>
2025-08-14 10:39:31 -0400

    [TSS-1894] Ignore network tokenization messages (#1)

--- lib/spreedly/transactions/network_tokenization.rb
+++ lib/spreedly/transactions/network_tokenization.rb
@@ -8,7 +8,16 @@ module Spreedly
     attr_reader :errors
 
     def initialize(xml_doc)
-      initialize_fields(xml_doc)
+      # initialize_fields(xml_doc)
+      # Override because initialize_fields ignores network_tokenization fields
+      self.class.fields.each do |field|
+        node = xml_doc.at_xpath(".//#{field}")
+        if node
+          value = node.inner_html.strip
+          instance_variable_set("@#{field}", value)
+        end
+      end
+
       @errors = errors_from(xml_doc)
     end
   end
